0b8816f10a80986e6d20e6ae963ba047efd9efc5
Alisa Zilberman <alisazilberman@Alisas-MacBook-Air.local>
2023-05-23 15:11:26 +0300

    add(array of nation population obj, display of table)

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiDataService } from '../api-data.service';
 import { nationPopulationObj } from '../nationPopulation';
 
@@ -8,12 +8,23 @@ import { nationPopulationObj } from '../nationPopulation';
   styleUrls: ['./table.component.scss'],
   providers: [ApiDataService]
 })
-export class TableComponent {
+export class TableComponent implements OnInit{
+
   constructor(private data:ApiDataService){}
+  
+  nationPopulationData: nationPopulationObj[] = []
 
   ngOnInit(){
+
     this.data.getData().subscribe((res:any)=>{
 
+      res.data.forEach((element:nationPopulationObj) => {
+
+          this.nationPopulationData.push(element);
+
+      });
+      
     })
   }
+  
 }
