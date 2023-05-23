26e39f80b5ebece14a4f53d3cf93943bd141e73d
Alisa Zilberman <alisazilberman@Alisas-MacBook-Air.local>
2023-05-23 14:58:23 +0300

    move(get data from app -> table component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-import { ApiDataService } from './api-data.service';
-import { nationPopulationObj } from './nationPopulation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [ApiDataService]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor(private data:ApiDataService){}
-
-  ngOnInit(){
-    this.data.getData().subscribe((res:any)=>{
-
-    })
-  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ApiDataService } from './api-data.service';
+import { nationPopulationObj } from './nationPopulation';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  providers: [ApiDataService]
 })
 export class TableComponent {
+  constructor(private data:ApiDataService){}
 
+  ngOnInit(){
+    this.data.getData().subscribe((res:any)=>{
+
+    })
+  }
 }
